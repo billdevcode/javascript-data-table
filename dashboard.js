45c2b470221417a23187bf640341ef01447c768f
Bill Deng <billdevcode@gmail.com>
2017-05-28 10:24:13 -0700

    Add toggleDataColumns to dashboard, separate from table and make function global

--- dashboard.js
+++ dashboard.js
@@ -1,29 +1,50 @@
 (() => {
   const body = document.body;
   const fragment = document.createDocumentFragment();
-  let div, form, input, label;
+  let div, span, form, input, label;
 
   const createDashboard = () => {
-    div = document.createElement('div');
-    div.classList.add('menu-dashboard');
-
+    div = document.getElementsByClassName('menu-dashboard')[0];
     form = document.createElement('form');
     div.appendChild(form);
 
     input = document.createElement('input');
     input.type = 'text';
+    input.placeholder = 'Search';
+    input.onkeyup = () => {
+      filterTable();
+    };
     input.classList.add('search-box');
-    form.appendChild(input);
-
-    input = document.createElement('input');
-    input.type = 'submit';
-    input.value = 'Search';
-    input.classList.add('submit-button');
     form.appendChild(input);
 
     fragment.appendChild(div);
     body.appendChild(fragment);
   }
+
+  const toggleDataColumns = (ev, key) => {
+    ev.stopPropagation();
+    let checkBox = ev.currentTarget;
+    let columnDatas = document.getElementsByClassName(`${key}-data`);
+    let columnHeading = document.getElementsByClassName(`${key}-heading`)[0];
+
+    if (checkBox.classList.contains(`data-toggle--hidden`)) {
+      columnHeading.style.display = '';
+      for (let i = 0; i < columnDatas.length; i++) {
+        columnDatas[i].style.display = '';
+      }
+      checkBox.classList.remove(`data-toggle--hidden`);
+      checkBox.classList.add(`data-toggle--visible`);
+    } else {
+      columnHeading.style.display = 'none';
+      for (let i = 0; i < columnDatas.length; i++) {
+        columnDatas[i].style.display = 'none';
+      }
+      checkBox.classList.remove(`data-toggle--visible`);
+      checkBox.classList.add(`data-toggle--hidden`);
+    }
+  }
+
+  window.toggleDataColumns = toggleDataColumns;
 
   createDashboard();
 
